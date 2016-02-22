3d2fefdbab9d97e3d889270761fd2107148020f6
Brice Howard <brice.d.howard@gmail.com>
2016-02-21 20:35:12 -0500

    Brice's commit

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+import os
+from pymongo import MongoClient
 app = Flask(__name__)
+
 @app.route("/")
 def main():
 	return render_template('index.html')
+
 if __name__ == "__main__":
 	app.run()
+
