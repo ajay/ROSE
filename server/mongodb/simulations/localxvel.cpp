d5c7a24ef2809609f627c5fe197977a01bc7e134
Cedric Blake <cedricblake48@gmail.com>
2016-03-15 16:31:51 -0400

    added an arm simulation and updated encoder simulation

--- server/mongodb/simulations/localxvel.cpp
+++ server/mongodb/simulations/localxvel.cpp
@@ -6,6 +6,8 @@ to the database "rosedb"
 currently, this script only simulates the encoder values by using
 a random number generator. the while loop breaks after 5 iterations
 
+stored within the "encoders" collection of the "rosedb" database
+
 */
 
 #include <iostream>
@@ -33,6 +35,13 @@ using bsoncxx::builder::stream::close_array;
 using bsoncxx::builder::stream::finalize;
 
 int main() {
+
+	//stores the values of the encoders corresponding to each wheel
+	int tl = 0;
+	int tr = 0;
+	int bl = 0;
+	int br = 0;
+
 	//used to create a client connection and connect to a mongo instance
 	mongocxx::instance inst{};
 	//NOTE: make sure to have "mongocxx::uri{}"
@@ -41,24 +50,22 @@ int main() {
 	auto db = conn["rosedb"];
 	auto encoders = db["encoders"];
 
+
+	//clears the collection on every reset
 	encoders.delete_many({});
 
+	//this document is used to reinitialize the position and velocity reading
+	//each time we need to restart the robot, as the current readings would
+	//now be out of date
 	bsoncxx::document::value initializer = 
-	document{} << "encoder values" << open_array
-		<< 0 << 0 
-		<< 0 << 0 
-		<< close_array << finalize;
+	document{} << "top left" << tl << "top right" << tr 
+	<< "bottom left" << bl << "bottom right" << br << finalize;
 
-	printf("Inserting first encoder value\n");
-
-	encoders.insert_one(std::move(initializer));
+	encoders.insert_one(move(initializer));
 
 
 
 	srand(time(NULL));
-
-	int displacement[4];
-	int d;
 
 	int count = 5;
 
@@ -71,20 +78,19 @@ int main() {
 		//and does not have multiple "$set" documents appended
 		bsoncxx::builder::stream::document updateVals;
 
-		for (int i = 0; i < 4; ++i) {
-			d = rand() % 100;
-			displacement[i] = d;
-			//cout<<displacement[i]<<endl;
-		}
+		tl = rand() % 100;
+		tr = rand() % 100;
+		bl = rand() % 100;
+		br = rand() % 100;
+
 
 		//update values here
 		updateVals << "$set" << open_document
-			<< "encoder values" << open_array
-			<< displacement[0] << displacement[1]
-			<< displacement[2] << displacement[3]
-			<< close_array << close_document;
+			<< "top left" << tl << "top right" << tr 
+			<< "bottom left" << bl << "bottom right" << br 
+			<< close_document;
 
-		encoderVals << "encoder values" << open_document
+		encoderVals << "top left" << open_document
 		<< "$exists" << true << close_document;
 
 		encoders.update_one(encoderVals.view(), updateVals.view());
