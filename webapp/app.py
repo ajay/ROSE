518b10f9f70423b6b1dd305a941f268887342412
Ajay Srivastava <ajaysriv3@gmail.com>
2016-04-01 06:18:39 -0400

    webapp changed to update by keys for mongo 2.4.9 compatibility

--- webapp/app.py
+++ webapp/app.py
@@ -6,26 +6,26 @@ def connect():
 # Substitute the 5 pieces of information you got when creating
 # the Mongo DB Database (underlined in red in the screenshots)
 # Obviously, do not store your password as plaintext in practice
-    #LOCALHOST
+    # LOCALHOST
     connection = MongoClient()
     handle = connection["rosedb"]
     return handle
-    #MONGOLAB
-    #connection = MongoClient("ds015878.mongolab.com", 15878)
-    #handle = connection["rosedb"]
-    #handle.authenticate("Brice","12345678")
-    #return handle
+    # MONGOLAB
+    # connection = MongoClient("ds015878.mongolab.com", 15878)
+    # handle = connection["rosedb"]
+    # handle.authenticate("Brice","12345678")
+    # return handle
 
 app = Flask(__name__)
 handle = connect()
 
-# Bind our index page to both www.domain.com/ 
-#and www.domain.com/index
+# Bind our index page to both www.domain.com/
+# and www.domain.com/index
 @app.route("/index" ,methods=['GET'])
 @app.route("/", methods=['GET'])
 def index():
     userinputs = [x for x in handle.mycollection.find()]
-    #speed = handle.mycollection.find({"_id":1})[0]["speed"]
+    # speed = handle.mycollection.find({"state":{"$exists":True}})[0]["speed"]
     return render_template('index.html', userinputs=userinputs)
 
 @app.route("/test", methods=['GET', 'POST'])
@@ -34,7 +34,7 @@ def test():
 	print request.method
 	#userinputs = [x for x in handle.mycollection.find()]
 	userinput = request.args.get("userinput")
-        oid = handle.mycollection.update({"_id":1},{"state":userinput},True)
+        oid = handle.mycollection.update({"state":{"$exists":True}},{"state":userinput},True)
         print userinput
         #userinputs = [x for x in handle.mycollection.find()]
 	#return render_template('index.html', userinputs=userinputs)
@@ -43,93 +43,93 @@ def test():
 @app.route("/write", methods=['POST'])
 def write():
     userinput = request.form.get("userinput")
-    oid = handle.mycollection.update({"_id":1},{"state":userinput}, True)
-    handle.mycollection.update({"_id":2},{"speed":0.0},True)
-    handle.mycollection.update({"_id":3},{"rotation":0},True);
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":userinput}, True)
+    handle.mycollection.update({"speed":{"$exists":True}},{"speed":0.0},True)
+    handle.mycollection.update({"rotation":{"$exists":True}},{"rotation":0},True);
     return redirect ("/")
-    
+
 #
-    
+
 @app.route("/north", methods=['POST'])
 def up():
-    oid = handle.mycollection.update({"_id":1},{"state":"NORTH"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}}, {"state":"NORTH"}, True)
     return redirect ("/")
 
 @app.route("/south", methods=['POST'])
 def down():
-    oid = handle.mycollection.update({"_id":1},{"state":"SOUTH"}, True)
-    return redirect ("/")    
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"SOUTH"}, True)
+    return redirect ("/")
 
 @app.route("/west", methods=['POST'])
 def left():
-    oid = handle.mycollection.update({"_id":1},{"state":"WEST"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"WEST"}, True)
     return redirect ("/")
 
 @app.route("/east", methods=['POST'])
 def right():
-    oid = handle.mycollection.update({"_id":1},{"state":"EAST"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"EAST"}, True)
     return redirect ("/")
 
 @app.route("/northwest", methods=['POST'])
 def northwest():
-    oid = handle.mycollection.update({"_id":1},{"state":"NORTHWEST"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"NORTHWEST"}, True)
     return redirect ("/")
 
 @app.route("/northeast", methods=['POST'])
 def northeast():
-    oid = handle.mycollection.update({"_id":1},{"state":"NORTHEAST"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"NORTHEAST"}, True)
     return redirect ("/")
 
 @app.route("/southwest", methods=['POST'])
 def southwest():
-    oid = handle.mycollection.update({"_id":1},{"state":"SOUTHWEST"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"SOUTHWEST"}, True)
     return redirect ("/")
 
 @app.route("/southeast", methods=['POST'])
 def southeast():
-    oid = handle.mycollection.update({"_id":1},{"state":"SOUTHEAST"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"SOUTHEAST"}, True)
     return redirect ("/")
 
 
 @app.route("/reset", methods=['POST'])
 def reset():
-    oid = handle.mycollection.update({"_id":1},{"state":"STOP"}, True)
+    oid = handle.mycollection.update({"state":{"$exists":True}},{"state":"STOP"}, True)
     return redirect ("/")
 
 @app.route("/speedup", methods=['POST'])
 def speedup():
-	print (handle.mycollection.find({"_id":2}) is not None)
-	if handle.mycollection.find({"_id":2}) is not None:
-		print ("speed:%f"%handle.mycollection.find({"_id":2})[0]["speed"])
-		spe = handle.mycollection.find({"_id":2})[0]["speed"] + 0.05 if handle.mycollection.find({"_id":2})[0]["speed"] <= 0.95 else handle.mycollection.find({"_id":2})[0]["speed"]
+	print (handle.mycollection.find({"speed":{"$exists":True}}) is not None)
+	if handle.mycollection.find({"speed":{"$exists":True}}) is not None:
+		print ("speed:%f"%handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"])
+		spe = handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"] + 0.05 if handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"] <= 0.95 else handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"]
 	else:
 		spe = 0.05
-	oid = handle.mycollection.update({"_id":2},{"speed":spe}, True)
+	oid = handle.mycollection.update({"speed":{"$exists":True}},{"speed":spe}, True)
 	return redirect ("/")
 
 @app.route("/speeddown", methods=['POST'])
 def speeddown():
-	if handle.mycollection.find({"_id":2}) is not None:
-		print ("speed:%f"%handle.mycollection.find({"_id":2})[0]["speed"])
-		spe = handle.mycollection.find({"_id":2})[0]["speed"] - 0.05 if handle.mycollection.find({"_id":2})[0]["speed"] >= 0.05 else handle.mycollection.find({"_id":2})[0]["speed"]
+	if handle.mycollection.find({"speed":{"$exists":True}}) is not None:
+		print ("speed:%f"%handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"])
+		spe = handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"] - 0.05 if handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"] >= 0.05 else handle.mycollection.find({"speed":{"$exists":True}})[0]["speed"]
 	else:
 		spe = 0
-	oid = handle.mycollection.update({"_id":2},{"speed":spe}, True)
+	oid = handle.mycollection.update({"speed":{"$exists":True}},{"speed":spe}, True)
 	return redirect ("/")
 
 @app.route("/rotateCW", methods = ['POST'])
 def rotateCW():
-    oid = handle.mycollection.update({"_id":3},{"rotation": 1}, True)
+    oid = handle.mycollection.update({"rotation":{"$exists":True}},{"rotation": 1}, True)
     return redirect ("/")
 
 @app.route("/rotateCCW", methods = ['POST'])
 def rotateCCW():
-    oid = handle.mycollection.update({"_id":3},{"rotation": -1}, True)
+    oid = handle.mycollection.update({"rotation":{"$exists":True}},{"rotation": -1}, True)
     return redirect ("/")
 
 @app.route("/rotatereset", methods = ['POST'])
 def rotatereset():
-    oid = handle.mycollection.update({"_id":3},{"rotation": 0}, True)
+    oid = handle.mycollection.update({"rotation":{"$exists":True}},{"rotation": 0}, True)
     return redirect ("/")
 
 def threadtest():
