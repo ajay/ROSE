4eff00329f293a8cfe45b281c824b35f9b8020ab
Neil Patel <neil.m.patel@rutgers.edu>
2016-04-29 02:00:31 -0400

    Displaying info on manager side, need to just separate data...

--- webapp/CustomerUI/app.py
+++ webapp/CustomerUI/app.py
@@ -44,5 +44,5 @@ def order(table, items, prices):
 # Remove the "debug=True" for production
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5010))
     app.run(host='0.0.0.0', port=port, debug=True)
